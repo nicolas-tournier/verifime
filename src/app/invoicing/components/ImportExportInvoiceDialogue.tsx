4bb195f8a12b1c4a5e66e00c42c5f8e1615bf3e3
nicolas-tournier <tourns@hotmail.com>
2024-04-08 10:03:17 +1000

    fix dialogue layout

--- src/app/invoicing/components/ImportExportInvoiceDialogue.tsx
+++ src/app/invoicing/components/ImportExportInvoiceDialogue.tsx
@@ -7,6 +7,7 @@ import {
     DialogActions,
     TextField
 } from "@mui/material";
+import Check from '@mui/icons-material/Check';
 import { useEffect, useState } from "react";
 
 export default function ImportExportInvoiceDialogue({ onOpenImportExportInvoiceDialogue, onCloseImportExportInvoiceDialogue }: { onOpenImportExportInvoiceDialogue: boolean, onCloseImportExportInvoiceDialogue: Function }) {
@@ -23,6 +24,11 @@ export default function ImportExportInvoiceDialogue({ onOpenImportExportInvoiceD
         setDialogueIsOpen(false);
         onCloseImportExportInvoiceDialogue();
     };
+
+    const handleOK = () => {
+        handleClose();
+        //TODO: Implement the import/export logic
+    }
 
     return (
         <Dialog open={dialogueIsOpen} fullWidth maxWidth="md">
@@ -45,9 +51,9 @@ export default function ImportExportInvoiceDialogue({ onOpenImportExportInvoiceD
                     }}
                 />
             </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleClose}>OK</Button>
+            <DialogActions sx={{ justifyContent: 'center', marginBottom: '1rem' }}>
+                <Button variant="contained" startIcon={<Check />} sx={{ width: '150px' }} onClick={handleOK} >OK</Button>
+                <Button variant="outlined" onClick={handleClose}>Cancel</Button>
             </DialogActions>
         </Dialog>
     )
